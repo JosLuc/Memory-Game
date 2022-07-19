33bf296b1ef2b5d519fde8407ba2e9de757f2cfc
Lucas Andrade <108074661+JosLuc@users.noreply.github.com>
2022-07-19 19:38:18 -0400

    Memory Game

---
+++ README.md
@@ -0,0 +1,21 @@
+# Jogo da Memória
+
+Jogo da memória com o tema pokémon;
+
+Este jogo é um desafio de projeto do bootcamp da DIO, porém fiz algumas mudanças, como por exemplo:
+- Coloquei uma tela de login;
+- Usei animações no background;
+- É possível colocar um nickname;
+
+Em breve pretendo trazer mais jogos e projetos diferentes para assim mostrar minha evolução.
+
+## Memory Game
+
+Pokémon-themed memory game;
+
+This game is a project challenge from the DIO bootcamp, however I made some changes such as:
+- I put a login screen;
+- I used animations in the background;
+- It's possible to put a nickname;
+
+Soon I intend to bring more games and different projects to show my evolution.
---
+++ assets/js/scripts.js
@@ -0,0 +1,80 @@
+const cards = document.querySelectorAll('.card');
+const spanPlayer = document.querySelector('.player');
+const disabledGame = document.querySelectorAll('.end');
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+
+//virar o card
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+//check card
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+//desabilitar o card
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//desvirar o card
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);
+}
+
+//reseta o tabuleiro
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//embaralhar os cards
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 20);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+//evento de click
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
+
+//nickname
+(window.pl4yer = () => {
+    spanPlayer.innerHTML = localStorage.getItem('player');
+})();
+
+
+
